0a4fb7af58dc8a46b1da0c32791f1fde5d6fba61
fainnerramirez <fainnerramirez@gmail.com>
2025-04-14 09:35:07 -0500

    feat: update manifest permission Storage api

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,12 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("La extensión ha sido instalada.");
 });
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    console.log(
+      `Storage key "${key}" in namespace "${namespace}" changed.`,
+      `Old value was "${oldValue}", new value is "${newValue}".`
+    );
+  }
+});
